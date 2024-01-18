fe24cfd5ee3f5f88aa24fcb7749ba66a19caa917
duyongquan <quandy2020@126.com>
2024-01-18 10:56:09 +0800

    fix
    Signed-off-by: duyongquan <quandy2020@126.com>

--- cmake/FindSphinx.cmake
+++
@@ -1,8 +0,0 @@
-find_program(SPHINX_EXECUTABLE
-             NAMES sphinx-build
-             PATHS
-               /usr/bin
-             DOC "Sphinx")
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Sphinx DEFAULT_MSG SPHINX_EXECUTABLE)
